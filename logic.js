617eaaa11231859b439559e1343552d83cd8ae32
Hamit <hamitsehjal26@gmail.com>
2022-11-27 01:01:46 -0500

    Added logic for changing the shape of menu icon

---
+++ logic.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    // menu.classList.toggle("fa-circle-xmark");
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+
+}
